7bdeb9e013ea16546fde1acef8af70d490973153
Willman Duffy <willman@workos.com>
2020-08-04 12:37:00 -0400

    Initial implementation of Directory Sync example app

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
 require 'sinatra'
 require 'workos'
 
-get '/groups' do
+get '/' do
+  @directories = WorkOS::DirectorySync.list_directories
 
+  erb :index
 end
 
-get '/users' do
+get '/directories/:id' do
+  @groups = WorkOS::DirectorySync.list_groups(directory: params[:id])
+  @users = WorkOS::DirectorySync.list_users(directory: params[:id])
 
+  erb :directory
 end
